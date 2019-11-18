811011d490e9d9fe5ceebaf689b0dd5bc497d856
Spenser Truex <web@spensertruex.com>
2019-11-17 17:51:37 -0800

    Add a package.el.

---
+++ coleslaw-pkg.el
@@ -0,0 +1,8 @@
+(define-package "coleslaw" "0.3.0" "Convenience for the Coleslaw static content Generator written in Common Lisp."
+  '((emacs "24.5")
+    (cl-lib ""))
+  :keywords
+  '("convenience" "tools" "wp")
+  :authors
+  '(("Spenser Truex" . "web@spensertruex.com"))
+  :url "https://spensertruex.com/coleslaw")
